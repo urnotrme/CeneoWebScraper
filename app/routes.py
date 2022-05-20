fb0e7f16acfa9e97a36ca1b8776d7eefa851fdb9
katwojuek <wojcikk@uek.krakow.pl>
2022-05-20 13:02:10 +0200

    2022-05-20 KW

---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index/<name>')
+def index(name="<<Hello World>>"):
+    return render_template("index.html", text=name)
