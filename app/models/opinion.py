5efb4af8b8c7d6511ac861a7284e764f504f86b7
urnotrme <s222466@student.uek.krakow.pl>
2022-06-10 13:53:25 +0200

    2022-06-10 AM

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,14 +1,23 @@
+from app.parameters import selectors
+from app.utils import get_item
+
 class Opinion():
-    def __init__(self, opinion):
-        self.selectors = {
-        "author": ["span.user-post__author-name"],
-        "recommendation": ["span.user-post__author-recomendation > em"],
-        "stars": ["span.user-post__score-count"],
-        "content": ["div.user-post__text"],
-        "useful": ["button.vote-yes > span"],
-        "useless": ["button.vote-no > span"],
-        "published": ["span.user-post__published > time:nth-child(1)", "datetime"],
-        "purchased": ["span.user-post__published > time:nth-child(2)", "datetime"],
-        "pros": ["div[class$=positives] ~ div.review-feature__item", None, True],
-        "cons": ["div[class$=negatives] ~ div.review-feature__item", None, True]
-        }
+    def __init__(self, author="", recommendation=None, stars=0, content="", useful=0, useless=0, publish_date=None, purchase_date=None, pros=[], cons=[], opinion_id=""):
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
+        self.pros = pros
+        self.cons = cons
+        self.opinion_id = opinion_id
+        return self
+    
+    def extract_opinion(self, opinion):
+        for key, value in selectors.items():
+            setattr(self, key, get_item(opinion, *value))
+        self.opinion_id = opinion["data-entry-id"]
+        return self
