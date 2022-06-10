15351fd02546da282cf0205cbcf2266425c9b6e1
urnotrme <s222466@student.uek.krakow.pl>
2022-06-10 08:14:00 +0200

    2022-06-09 AM

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,3 +1,14 @@
 class Opinion():
-    def __init__():
-        pass
+    def __init__(self, opinion):
+        self.selectors = {
+        "author": ["span.user-post__author-name"],
+        "recommendation": ["span.user-post__author-recomendation > em"],
+        "stars": ["span.user-post__score-count"],
+        "content": ["div.user-post__text"],
+        "useful": ["button.vote-yes > span"],
+        "useless": ["button.vote-no > span"],
+        "published": ["span.user-post__published > time:nth-child(1)", "datetime"],
+        "purchased": ["span.user-post__published > time:nth-child(2)", "datetime"],
+        "pros": ["div[class$=positives] ~ div.review-feature__item", None, True],
+        "cons": ["div[class$=negatives] ~ div.review-feature__item", None, True]
+        }
